ac5229d77abd6f508ae866b4e2c730496f877213
guiachcar <guiachcar@hotmail.com>
2017-05-06 18:59:08 -0300

    testes dadasda

--- ConsultiBETA/ConsultiBETA/Form1.Designer.cs
+++ ConsultiBETA/ConsultiBETA/Form1.Designer.cs
@@ -65,7 +65,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(487, 202);
+            this.button2.Location = new System.Drawing.Point(370, 238);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
